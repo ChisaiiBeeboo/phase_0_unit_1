934a4da047d60b3c308883347451610b07852ab3
Kate's Macbook Air <macbookair@Kate-MacBook-Air.local>
2014-05-11 20:06:06 -0700

    finished mission game and group project

--- week_3/5_design_cc_mission/my_solution.js
+++ week_3/5_design_cc_mission/my_solution.js
@@ -1,37 +1,119 @@
-// I worked [by myself, with:] on this challenge
+// I worked by myself on this challenge
 
 // Your mission description:
-// 
-// 
+// Create a snowman!
+// Take him somewhere fun to play!
 // 
 
 // Pseudocode
-// 
-// 
-// 
-// 
-// 
+// confirm that user indeed want to build a snowman
+// prompt the user for a second input
+// prompt user to create snowman with properties name, eyes and nose
+// include hereHeIs function property to show off his attributes
+// call function
+// prompt user to take snowMan to the beach or snow palace
+// if user input is 'beach' alert user that it's "game over"
+// else if user inputer is 'snow palace' snowMan has fun all over the place
+// else any other input will result in game over
+// alert game is over
 
 // Initial Code
 
+confirm("Do you want to build a snowman?");
 
 
+var wannaPlay = prompt("Do you want to go and play?");
+	
+	if (wannaPlay === "yes"){
+		console.log("YAAAAaaaaay");
+	} else {
+		alert("OOOoooh Kaaaay baaiiii =/");
+	};
+
+var snowMan = {
+	name: prompt("What should we name our snowman?"),
+	eyes: prompt("Should we use pebbles or an Oreo for his eyes"),
+	nose: prompt("And here's a ____ (rhymes with parrot, starts with a C) for a nose!"),
+	hereHeIs: function() {console.log("This is " + snowMan.name + " our snowman with " + snowMan.eyes + " eyes and a " + snowMan.nose + " nose (:^)_)_) ")}
+};
+
+snowMan.hereHeIs()
+
+
+var	playPlace = prompt("Should we take " + snowMan.name + " to the beach or the snow palace to play?")
+	if (playPlace === "beach") {
+		alert(snowMan.name + " has melted!!");
+		alert("GAME OVER");
+	} else if ( playPlace === "snow palace" )  {
+		console.log( "Look at the yellow snow! Look how much fun he's having!" );
+		alert("WEEEEeeeee (:^)_)_)")
+	} else {
+		console.log( snowMan.name + " can't go there!!");
+		alert(snowMan.name + " can't go there, he's a snowman! " + snowMan.name + " is iced water now. GAME OVER.");
+	}
+
+
+alert("OOOoooh Kaaaay baaiiii =/");
 
 
 
 // Refactored Code
 
+confirm("Do you want to build a snowman?");
 
 
+var Game = function() {
+	wannaPlay = prompt("Do you want to go and play?");
+	
+	if (wannaPlay === "yes"){
+		console.log("YAAAAaaaaay");
+	
+
+	var snowMan = {
+		name: prompt("What should we name our snowman?"),
+		eyes: prompt("Should we use pebbles or an Oreo for his eyes"),
+		nose: prompt("And here's a ____ (rhymes with parrot, starts with a C) for a nose!"),
+		hereHeIs: function() {console.log("This is " + snowMan.name + " our snowman with " + snowMan.eyes + " eyes and a " + snowMan.nose + " nose (:^)_)_) ")}
+	};
+
+	snowMan.hereHeIs()
 
 
+	var	playPlace = prompt("Should we take " + snowMan.name + " to the beach or the snow palace to play?")
+		if (playPlace === "beach") {
+			alert(snowMan.name + " has melted!!");
+			alert("GAME OVER");
+		} else if ( playPlace === "snow palace" )  {
+			console.log( "Look at the yellow snow! Look how much fun he's having!" );
+			alert("WEEEEeeeee (:^)_)_)");
+			alert("OOOoooh Kaaaay baaiiii =/");
+		} else {
+			console.log( snowMan.name + " can't go there!!");
+			alert(snowMan.name + " can't go there, he's a snowman! " + snowMan.name + " is iced water now. GAME OVER.");
+		}
+
+		}
+else {
+		alert("OOOoooh Kaaaay baaiiii =/");
+	}
+
+};
+
+Game();
+
+
+// Release 5: Review other's solutions
+//britneyroar/phase_0_unit_1
+//BrennonTWilliams/phase_0_unit_1
 
 // Reflection
 // 
-// 
-// 
-// 
-// 
+// This was a really fun challenge as it combined coding and my nieces' favorite Disney movie. I started out 
+// building a simple yes or no input game and later incorporated object literal construction to have the 
+// user create a snowMan object. I'm still plagued with the usual syntax errors as I'm coding but notice that I'm recognizing 
+// the error's now and am able to identify them faster. I also had to restructure my code to get the game to end when
+// the user input 'no' when asked whether they "want to go and play". In all I was able to apply a lot of the things
+// I learned this week into the game, feeling very satisfied with the final product. 
 // 
 // 
 // 
---
+++ week_3/6_group_project/my_solution.js
@@ -0,0 +1,17 @@
+Release 5: Reflect
+
+What was this experience like?
+This so far has been my favorite challenge. I enjoy coding and was lucky to be the person assigned
+to translate pseudocode into actual code. The members of my group did a great job of communicating and 
+promptly finishing their portion of the challenge and passing it on. Austin was even kind enough to research 
+functions he referenced and provide supplemental links. I had adequate time to analzye the pseudocode and learn new 
+JavaScript functions and concepts. 
+
+
+Was your group successful in passing the tests?
+Passed with flying colors.
+
+What did you learn about writing user stories, pseudocoding, writing code, refactoring, etc.?
+It's good practice to get a solid idea of what you're looking to accomplish in non-technical plain english. Once 
+you have your idea and goals down the next step obvious is to translate your idea's into JavaScript concepts. Pseudocode
+gives you a solid guide to accomplishing your goals.
